c6a1f297e3faec5e64d3dddf6a92fb5dc05bb378
yash gahlot <71267826+yashgahlot@users.noreply.github.com>
2025-02-03 17:35:50 -0400

    Update triangle.js
    changed colors of the triangle.

--- triangle.js
+++ triangle.js
@@ -16,10 +16,10 @@ window.onload = function init()
      ];
       
     var colors = [
-        vec4(1.0, 0.0, 0.0, 1.0),  // red
-        vec4(0.0, 0.0, 1.0, 1.0), //blue
-        vec4(0.0, 1.0, 0.0, 1.0),  // green
-        vec4(1.0, 0.0, 0.0, 1.0),  //red
+    vec4(1.0, 1.0, 0.0, 1.0),  // yellow
+    vec4(1.0, 0.5, 0.0, 1.0),  // orange
+    vec4(0.5, 0.0, 0.5, 1.0),  // purple
+    vec4(0.0, 1.0, 1.0, 1.0),  // cyan
     ]
 
 
